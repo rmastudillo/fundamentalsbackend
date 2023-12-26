d663f9dc3fd812ce8baf7cc13cb7db7f798288ac
richi <rmastudillo@uc.cl>
2023-12-26 00:03:42 -0300

    muchos cambios

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # fundamentalsbackend
+# Commands
+```
+uvicorn app.main:app --reload
+pip install -r requirements.txt
+source venv/bin/activate
+python3 -m venv venv
+```
--- app/types/questions.py
+++ app/types/questions.py
@@ -1,62 +1,66 @@
 from typing import List, Optional
-
-from app.types.answers import Answer  # Asegúrate de que esta ruta es correcta
 from bson import ObjectId
-from pydantic import BaseModel, Field, root_validator, validator
+from pydantic import BaseModel, Field, validator
 
+# Revisa la ruta del importe de Answer para asegurarte de que es correcta
+from app.types.answers import Answer
 
 # Clase PyObjectId para convertir ObjectId de MongoDB a un formato compatible con Pydantic
+
+
 class PyObjectId(ObjectId):
     @classmethod
     def __get_validators__(cls):
         yield cls.validate
 
     @classmethod
-    def validate(cls, v):
+    def validate(cls, v, handler):
         if not ObjectId.is_valid(v):
             raise ValueError("Invalid ObjectId")
         return ObjectId(v)
 
     @classmethod
-    def __modify_schema__(cls, field_schema):
-        field_schema.update(type="string")
-
+    def __get_pydantic_json_schema__(cls, schema_or_field, current_handler):
+        # Actualizar el esquema JSON para el tipo personalizado
+        return {"type": "string"}
 
 # Modelo para representar una pregunta y sus respuestas
+
+
 class Question(BaseModel):
-    id: Optional[PyObjectId] = Field(alias="_id")
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
     module: str
     topic: str
     course: str
     source: str
     content: str
     question_text: str
-    answer_options: List[Answer]  # Lista de respuestas (modelo Answer)
-    correct_answer_index: Optional[int] = None  # Índice de la respuesta correcta
+    answer_options: List[Answer]
+    # Índice de la respuesta correcta
+    correct_answer_index: Optional[int] = None
     hint_list: List[str]
     answer_solved_list: List[str]
     difficulty: str
 
     class Config:
-        arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
-        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        exclude_unset = True
+        populate_by_name = True
 
-    # Validador para asignar IDs automáticamente a las respuestas y determinar la correcta
     @validator("answer_options", pre=True)
     def process_answers(cls, answers):
         for index, answer in enumerate(answers):
             if isinstance(answer, dict):  # Convertir dict a Answer si es necesario
-                answer["id"] = index + 1  # Asignar ID basado en la posición
+                answer['id'] = index + 1  # Asignar ID basado en la posición
         return answers
 
-    @root_validator
-    def set_correct_answer_index(cls, values):
-        answers = values.get("answer_options", [])
+    @validator("correct_answer_index", always=True)
+    def set_correct_answer_index(cls, v, values):
+        answers = values.get('answer_options', [])
         correct_index = None
         for index, answer in enumerate(answers):
-            if answer.is_correct:
+            if isinstance(answer, Answer) and answer.is_correct:
                 correct_index = index + 1
                 break  # Asumimos que solo hay una respuesta correcta
-        values["correct_answer_index"] = correct_index
-        return values
+        return correct_index if correct_index is not None else v
